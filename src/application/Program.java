da41a01a322c479ea1ee29f4143a03978adc95fc
Bianka Costa <biankacostads@gmail.com>
2024-06-17 16:56:26 -0300

    Program created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,42 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Room number: ");
+		int number = sc.nextInt();
+		System.out.println("Check-in date (dd/mm/YYYY): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.println("Check-out date (dd/mm/YYYY): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		Reservation reservation = new Reservation(number, checkIn, checkOut);
+		System.out.println("Reservation: " + reservation);
+		
+		System.out.println();
+		System.out.println("Enter data to update reservation: ");
+		System.out.println("Check-in date (dd/mm/YYYY): ");
+		checkIn = sdf.parse(sc.next());
+		System.out.println("Check-out date (dd/mm/YYYY): ");
+		checkOut = sdf.parse(sc.next());
+		
+		reservation.updateDates(checkIn, checkOut);
+		System.out.println("Reservation: " + reservation);
+		
+		
+		sc.close();
 
 	}
 
